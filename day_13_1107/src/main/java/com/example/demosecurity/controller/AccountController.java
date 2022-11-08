304a45408641db0dc54788223ad56aa5d96ca63f
Tudang1 <teedangtu@gmail.com>
2022-11-08 21:29:58 +0700

    Update AccountController.java

--- day_13_1107/src/main/java/com/example/demosecurity/controller/AccountController.java
+++ day_13_1107/src/main/java/com/example/demosecurity/controller/AccountController.java
@@ -4,7 +4,9 @@ import com.example.demosecurity.dto.AccountDto;
 import com.example.demosecurity.entity.Account;
 import com.example.demosecurity.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,10 +21,11 @@ public class AccountController {
     public ResponseEntity<?> save(@RequestBody AccountDto accountDto){
         Account account = new Account();
         account.setUserName(accountDto.getUserName());
-        String passwordEn =
-        account.setPassword(accountDto.getPassword());
+        String passwordEn = BCrypt.hashpw(accountDto.getPassword(),BCrypt.gensalt(12));
+        account.setPassword(passwordEn);
         account.setFullName(accountDto.getFullName());
+        accountRepository.save(account);
 
-        return accountRepository.save(account);
+        return new ResponseEntity<>("Save successful", HttpStatus.CREATED);
     }
 }
